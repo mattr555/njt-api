89531abf60a0b4f1f0d0e21180be66c95c2dda08
Matthew Ramina <mattramina@gmail.com>
2014-06-20 18:04:54 -0400

    new modular structure to support other gtfs feeds

---
+++ agencies/njt.py
@@ -0,0 +1,80 @@
+from base import Base
+import os
+import json
+import urllib2
+import lxml.html
+
+class NJT(Base):
+	def __init__(self):
+		self.name = 'njt'
+		self.station_replacements = {
+			"princeton junction": "PRINCETON JCT.",
+			"philadelphia": "30TH ST. PHL.",
+			"montclair state university": "MSU",
+			"montclair state": "MSU",
+			"jersey ave": "JERSEY AVE."
+		}
+		self.normalize_replacements = ["HBLR", "DvLR", "th", "st", "nd", "MSU"]
+		super(NJT, self).__init__()
+
+	def parse(self):
+		import csv
+
+		super(NJT, self).parse()
+		departurevision = {}
+		with open(os.path.join(self.datadir, 'departurevision.csv')) as f:
+			for station, code in csv.reader(f):
+				matches = []
+				for k in self.stops:
+					if station.lower() == k.lower():
+						departurevision[k] = code
+						break
+					elif station.lower() in k.lower():
+						matches.append(k)
+				else:
+					if len(matches) == 1:
+						departurevision[matches[0]] = code
+					elif matches:
+						print 'Multiple found for', station, code
+						for i, j in enumerate(matches):
+							print i, j
+						departurevision[matches[int(raw_input('? '))]] = code
+					else:
+						print 'None found for', station, code
+						departurevision[raw_input('? ')] = code
+
+		with open(os.path.join(self.datadir, 'dv'), 'w') as f:
+			json.dump(departurevision, f)
+
+		self.departurevision = departurevision
+
+	def load(self):
+		super(NJT, self).load()
+		with open(os.path.join(self.datadir, 'dv')) as f:
+			self.departurevision = json.load(f)
+
+	def get_station_page(self, station):
+		station = self.departurevision.get(station)
+		if not station:
+			return None, None
+		url = 'http://dv.njtransit.com/mobile/tid-mobile.aspx?sid=' + station
+		return urllib2.urlopen(url).read(), url
+
+	def _get_departure_rows(self, station_page):
+		h = lxml.html.document_fromstring(station_page)
+		t = h.get_element_by_id('GridView1')
+		for row in t.getchildren()[1:]:
+			data = row.getchildren()[0].getchildren()[1].getchildren()[0].getchildren() #fuckin njt
+			yield data
+
+	def get_status(self, train, station_page):
+		for data in self._get_departure_rows(station_page):
+			if train.lstrip('0') == data[4].text_content().strip():
+				return data[5].text_content().strip(), data[2].text_content().strip()
+		return '', ''
+
+	def train_list(self, station_page):
+		l = []
+		for data in self._get_departure_rows(station_page):
+			l.append(data[4].text_content().strip())
+		return l
--- api.py
+++ api.py
@@ -1,10 +1,10 @@
 import webapp2
-from data import routes, stops, dates, departurevision, station_replacements
 import datetime
 import json
-import lxml.html
-import urllib2
-import logging
+
+from agencies.njt import NJT
+
+njt = NJT()
 
 def nth(year, month, n, weekday):
 	for i in range(((n-1)*7+1), n*7+1):
@@ -28,32 +28,7 @@ class Handler(webapp2.RequestHandler):
     def write(self, *a, **kw):
         self.response.write(*a, **kw)
 
-def get_station_page(station):
-	station = departurevision.get(station)
-	if not station:
-		return None, None
-	url = 'http://dv.njtransit.com/mobile/tid-mobile.aspx?sid=' + station
-	return urllib2.urlopen(url).read(), url
-
-def get_status(train, station_page):
-	h = lxml.html.document_fromstring(station_page)
-	t = h.get_element_by_id('GridView1')
-	for row in t.getchildren()[1:]:
-		data = row.getchildren()[0].getchildren()[1].getchildren()[0].getchildren() #fuckin njt
-		if train.lstrip('0') == data[4].text_content().strip():
-			return data[5].text_content().strip(), data[2].text_content().strip()
-	return '', ''
-
-def train_list(station_page):
-	h = lxml.html.document_fromstring(station_page)
-	t = h.get_element_by_id('GridView1')
-	l = []
-	for row in t.getchildren()[1:]:
-		data = row.getchildren()[0].getchildren()[1].getchildren()[0].getchildren() #fuckin njt
-		l.append(data[4].text_content().strip())
-	return l
-
-def find_stop(s):
+def find_stop(s, stops, station_replacements):
 	s = s.lower()
 	if s in station_replacements:
 		return station_replacements[s]
@@ -67,7 +42,7 @@ def find_stop(s):
 		return str(matches[0])
 	return list(map(str, matches))
 
-def infer_stop(known, unknown):
+def infer_stop(known, unknown, stops):
 	known_routes = set(stops[known])
 	unknown_routes = []
 	for i in unknown:
@@ -77,114 +52,115 @@ def infer_stop(known, unknown):
 			return stop, list(known_routes.intersection(iroutes))
 	return None, None
 
-def normalize_stop_name(s):
-	replacements = ["HBLR", "DvLR", "th", "st", "nd", "MSU"]
-	s = s.title()
-	for i in replacements:
-		if i.title() in s:
-			s = s.replace(i.title(), i)
-	return s.replace('station', "Station")
+
+def get_response(agency, orig, dest):
+	# step 1: figure out which stop they meant
+	orig = orig.replace('-', ' ')
+	dest = dest.replace('-', ' ')
+	nowt = datetime.datetime.now(EDT())
+	now = nowt.strftime('%H:%M:%S')
+	if nowt.hour < 4:
+		nowt -= datetime.timedelta(days=1)
+	today = nowt.strftime('%Y%m%d')
+	route_matches, trains = [], []
+	resp = {'failed': True, 'routes': []}
+		
+	orig_eq = find_stop(orig, agency.stops, agency.station_replacements)
+	dest_eq = find_stop(dest, agency.stops, agency.station_replacements)
+
+	if (type(orig_eq) is str) and (type(dest_eq) is str):
+		orig = orig_eq
+		dest = dest_eq
+		orig_routes = set(agency.stops[orig])
+		dest_routes = set(agency.stops[dest])
+		route_matches = list(orig_routes.intersection(dest_routes))
+	elif type(orig_eq) is list and type(dest_eq) is str:
+		dest = dest_eq
+		orig, route_matches = infer_stop(dest, orig_eq, agency.stops)
+	elif type(dest_eq) is list and type(orig_eq) is str:
+		orig = orig_eq
+		dest, route_matches = infer_stop(orig, dest_eq, agency.stops)
+
+	# step 2: look through each route and find the trains that go there
+	if route_matches:
+		status_page, station_url = agency.get_station_page(orig)
+		status_page_trains = agency.train_list(status_page) if status_page else []
+		for route in route_matches:
+			# get the stop times given the direction
+			route = agency.routes[route]
+			direction = None
+			for i in route['route']:
+				if i['stop_name'] == orig:
+					if not direction:
+						direction = '0'
+					orig_times = i['times'][direction]
+				elif i['stop_name'] == dest:
+					if not direction:
+						direction = '1'
+					dest_times = i['times'][direction]
+
+			# filter the times for the given day
+			periods = agency.dates[today]
+			orig_real_times = []
+			dest_real_times = []
+			for i in periods:
+				if orig_times.get(i):
+					orig_real_times += orig_times[i]
+					dest_real_times += dest_times[i]
+
+			orig_real_times.sort()
+			dest_real_times.sort()
+
+			# get the next 3 trains for each route
+			train_count = 0
+			while train_count < 3:
+				dep_time, dep_trip, arr_time, dep_train = (None,)*4
+				for i in range(len(orig_real_times)):
+					time, trip, train = orig_real_times.pop(0)
+					if now < time or train in status_page_trains: #see if the train is delayed
+						dep_time = time
+						dep_trip = trip
+						dep_train = train
+						trains.append(train)
+						break
+				for i, j, _ in dest_real_times:
+					if j == dep_trip: #make sure the train actually stops on this trip
+						arr_time = i
+				if arr_time:
+					resp['origin'] = agency.normalize_stop_name(orig)
+					resp['destination'] = agency.normalize_stop_name(dest)
+					resp['failed'] = False
+					resp['url'] = station_url
+					status, track = agency.get_status(dep_train, status_page) if status_page else ('', '')
+					resp['routes'].append({'line': route['name'], 'departure_time': dep_time, 
+						'arrival_time': arr_time, 'train': dep_train, 'status': status.title(), 'track': track})
+					train_count += 1
+				if not orig_real_times:
+					break
+
+	return resp
 
 class MainPage(Handler):
 	def get(self):
 		self.write('This api is meant for DDG, but you can use it too...\r\nendpoint is /times/:from/:to, delimit spaces with -')
 
-class Times(Handler):
-	def get(self, orig, dest):
-		# step 1: figure out which stop they meant
-		orig = orig.replace('-', ' ')
-		dest = dest.replace('-', ' ')
-		nowt = datetime.datetime.now(EDT())
-		now = nowt.strftime('%H:%M:%S')
-		if nowt.hour < 4:
-			nowt -= datetime.timedelta(days=1)
-		today = nowt.strftime('%Y%m%d')
-		route_matches, trains = [], []
-		resp = {'failed': True, 'routes': []}
-		
-		orig_eq = find_stop(orig)
-		dest_eq = find_stop(dest)
+def Times(agency):
+	class TheHandler(Handler):
+		def get(self, orig, dest):
+			resp = get_response(agency, orig, dest)
+			self.response.headers.add_header('Content-Type', 'application/json')
+			self.write(json.dumps(resp))
+	return TheHandler
 
-		if (type(orig_eq) is str) and (type(dest_eq) is str):
-			orig = orig_eq
-			dest = dest_eq
-			orig_routes = set(stops[orig])
-			dest_routes = set(stops[dest])
-			route_matches = list(orig_routes.intersection(dest_routes))
-		elif type(orig_eq) is list and type(dest_eq) is str:
-			dest = dest_eq
-			orig, route_matches = infer_stop(dest, orig_eq)
-		elif type(dest_eq) is list and type(orig_eq) is str:
-			orig = orig_eq
-			dest, route_matches = infer_stop(orig, dest_eq)
-
-		# step 2: look through each route and find the trains that go there
-		if route_matches:
-			status_page, station_url = get_station_page(orig)
-			status_page_trains = train_list(status_page) if status_page else []
-			for route in route_matches:
-				# get the stop times given the direction
-				route = routes[route]
-				direction = None
-				for i in route['route']:
-					if i['stop_name'] == orig:
-						if not direction:
-							direction = '0'
-						orig_times = i['times'][direction]
-					elif i['stop_name'] == dest:
-						if not direction:
-							direction = '1'
-						dest_times = i['times'][direction]
-
-				# filter the times for the given day
-				periods = dates[today]
-				orig_real_times = []
-				dest_real_times = []
-				for i in periods:
-					if orig_times.get(i):
-						orig_real_times += orig_times[i]
-						dest_real_times += dest_times[i]
-
-				orig_real_times.sort()
-				dest_real_times.sort()
-
-				# get the next 3 trains for each route
-				train_count = 0
-				while train_count < 3:
-					dep_time, dep_trip, arr_time, dep_train = (None,)*4
-					for i in range(len(orig_real_times)):
-						time, trip, train = orig_real_times.pop(0)
-						if now < time or train in status_page_trains: #see if the train is delayed
-							dep_time = time
-							dep_trip = trip
-							dep_train = train
-							trains.append(train)
-							break
-					for i, j, _ in dest_real_times:
-						if j == dep_trip: #make sure the train actually stops on this trip
-							arr_time = i
-					if arr_time:
-						resp['origin'] = normalize_stop_name(orig)
-						resp['destination'] = normalize_stop_name(dest)
-						resp['failed'] = False
-						resp['url'] = station_url
-						status, track = get_status(dep_train, status_page) if status_page else ('', '')
-						resp['routes'].append({'line': route['name'], 'departure_time': dep_time, 
-							'arrival_time': arr_time, 'train': dep_train, 'status': status.title(), 'track': track})
-						train_count += 1
-					if not orig_real_times:
-						break
-
-		self.response.headers.add_header('Content-Type', 'application/json')
-		self.write(json.dumps(resp))
-
-class Stops(Handler):
-	def get(self):
-		self.response.headers.add_header('Content-Type', 'text/plain')
-		self.write('\r\n'.join(stops.keys() + station_replacements.keys()))
+def Stops(agency):
+	class TheHandler(Handler):
+		def get(self):
+			self.response.headers.add_header('Content-Type', 'text/plain')
+			self.write('\r\n'.join(agency.stops.keys() + agency.station_replacements.keys()))
+	return TheHandler
 
 application = webapp2.WSGIApplication([
     ('/', MainPage),
-    ('/times/([\w-]+)/([\w-]+)', Times),
-    ('/stops', Stops)
+    ('/njt/times/([\w-]+)/([\w-]+)', Times(njt)),
+    ('/njt/stops', Stops(njt))
 ], debug=True)
